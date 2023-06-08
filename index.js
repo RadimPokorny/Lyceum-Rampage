075262fb45af0b03ddc59ad4e322fb57b9990df4
kingradas <pokornyr.04@spst.eu>
2023-06-08 18:05:46 +0200

    goofyayay
    :))

--- index.js
+++ index.js
@@ -25,36 +25,3 @@ function changeIcon(){
 
 }
 
-function handleMediaQueries() {
-    if (window.matchMedia("(max-width: 991px)").matches) {
-      // Změny pro média s maximální šířkou 991px
-      document.querySelector(".content-desc").style.fontSize = "18px";
-    } else {
-      // Zrušení změn pro média s maximální šířkou 991px
-      document.querySelector(".content-desc").style.fontSize = "";
-    }
-  
-    if (window.matchMedia("(max-width: 768px)").matches) {
-      // Změny pro média s maximální šířkou 768px
-      document.querySelector(".header-left").style.display = "none";
-      document.querySelector(".open-div").style.display = "flex";
-      document.querySelector(".content").style.width = "100%";
-      document.querySelector(".content-showcase").style.width = "100%";
-      document.querySelector(".content-desc").style.width = "80%";
-      document.querySelector(".content-contacts").style.width = "100%";
-    } else {
-      // Zrušení změn pro média s maximální šířkou 768px
-      document.querySelector(".header-left").style.display = "";
-      document.querySelector(".open-div").style.display = "";
-      document.querySelector(".content").style.width = "";
-      document.querySelector(".content-showcase").style.width = "";
-      document.querySelector(".content-desc").style.width = "";
-      document.querySelector(".content-contacts").style.width = "";
-    }
-  }
-  
-  // Spuštění inicializace
-  handleMediaQueries();
-  
-  // Přidání posluchače události resize pro okno
-  window.addEventListener("resize", handleMediaQueries);
